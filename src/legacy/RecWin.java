b62142b5d3695375949d2f4dc6d0d1a7af96cb14
CalebFurley <caleb.furley@gmail.com>
2024-01-27 15:31:13 -0600

    RecWin Legacy Fixed.

--- src/legacy/RecWin.java
+++ src/legacy/RecWin.java
@@ -1,7 +1,6 @@
 package legacy;
 import java.awt.Color;
 import java.awt.GridLayout;
-import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
@@ -9,7 +8,6 @@ import java.io.IOException;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.UnsupportedAudioFileException;
 import javax.swing.BorderFactory;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
