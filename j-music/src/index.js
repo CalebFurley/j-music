26f01eaa51d7e8dfe0a856ed4e77330dc266b027
cfurley <caleb.furley@gmail.com>
2025-12-12 23:45:33 -0600

    rebuild project

--- j-music/src/index.js
+++
@@ -1,6 +0,0 @@
-exports.app = require('./app');
-exports.db = require('./db');
-exports.AppConfig = require('./AppConfig');
-exports.entities = require('./entities');
-exports.features = require('./features');
-exports.integrations = require('./integrations');
